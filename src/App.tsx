9405da42f36dcfb6680d07b90f8850d927226790
WONILLISM <wonillism@gmail.com>
2023-05-02 15:33:12 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Dashboard from "./pages/Dashboard";
-import GoogleSheets from "./pages/GoogleSheets";
 
 const { VITE_GOOGLE_OAUTH_CLIENT_ID } = import.meta.env;
 
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/sheets" element={<GoogleSheets />} />
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>
