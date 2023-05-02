4812008ef361d4937a6794fa061a785cfac195ed
WONILLISM <wonillism@gmail.com>
2023-05-02 14:13:44 +0900

    [feat] add youtube subscription list (#6)

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -1,12 +1,16 @@
 import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
 
 const GoogleLogin = () => {
+  const navigate = useNavigate();
   const login = useGoogleLogin({
     onSuccess: (res) => {
-      console.log(res);
+      // console.log(res);
       localStorage.setItem("access-token", res.access_token);
+      navigate("/dashboard");
     },
-    scope: "https://www.googleapis.com/auth/youtube",
+    scope:
+      "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/spreadsheets",
   });
   return <button onClick={() => login()}>google login</button>;
 };
