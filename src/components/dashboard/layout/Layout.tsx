43368b3755c1b338338db951543f05380114ce80
WONILLISM <wonillism@gmail.com>
2023-05-10 19:55:42 +0900

    [feat] add dashboard sidebar #21
    사이드바 토글 기능 추가, 스타일 적용

--- src/components/dashboard/layout/Layout.tsx
+++ src/components/dashboard/layout/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { useState } from "react";
 
 const WEB_HEIGHT = 56;
 
-const MainWrapper = styled.div`
+const MainWrapper = styled.div<{ ml: number }>`
   margin-top: ${WEB_HEIGHT}px;
+  margin-left: ${(props) => props.ml}px;
   display: flex;
 `;
 
@@ -16,11 +18,17 @@ const RootStyle = styled.div`
 `;
 
 const Layout = () => {
+  const [openSidebar, setOpenSidebar] = useState<boolean>(false);
+
+  const handleSidebarOpen = () => {
+    setOpenSidebar(!openSidebar);
+  };
+
   return (
     <RootStyle>
-      <Header />
-      <MainWrapper>
-        <Sidebar />
+      <Header handleSidebarOpen={handleSidebarOpen} />
+      <Sidebar open={openSidebar} />
+      <MainWrapper ml={openSidebar ? 160 : 72}>
         <Outlet />
       </MainWrapper>
     </RootStyle>
